9e234bce2017bce518b63524bdc613695fcdd35e
irgiastadhani334 <irgiastadhani420@gmail.com>
2026-03-16 16:45:49 +0700

    Update main.cpp
    Adding proper main.cpp

--- Task 1/main.cpp
+++ Task 1/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include "RunSession.h"
+#include "FixedInputGenerator.h"
+#include "BasicScoringRule.h"
+#include "BasicRewardRule.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    FixedInputGenerator inputGen;
+    BasicScoringRule    scoringRule;
+    BasicRewardRule     rewardRule;
+
+    RunSession session(&inputGen, &scoringRule, &rewardRule, 3);
+    session.start();
+
     return 0;
 }
